9eb43768b62565e8b9dd75a112f79c2d891ab60f
Ryo Murakami <integral14@gmail.com>
2014-11-16 01:18:43 +0900

    add ImageHolder

--- camera-server/camera-server.py
+++ camera-server/camera-server.py
@@ -17,6 +17,27 @@ gflags.DEFINE_string("ui_server_url", "http://localhost:5001", "ui_server_url")
 FLAGS = gflags.FLAGS
 
 
+class ImageHolder(object):
+
+    def __init__(self):
+        self._buffer = dict()
+        self._lock = threading.Lock()
+        with open("./static/img/default.jpg", "rb") as f:
+            self._default_image = f.read()
+
+    def push(self, index, buf):
+        with self._lock:
+            self._buffer[index] = buf
+
+    def pop(self, index):
+        if index not in self._buffer:
+            return self._default_image
+        with self._lock:
+            buf = self._buffer[index]
+            self._buffer[index] = None
+            return buf
+
+
 class HttpHandler(tornado.web.RequestHandler):
     """HTTPのハンドラ
 
@@ -40,7 +61,7 @@ class WSPopHandler(tornado.websocket.WebSocketHandler):
     loop関数の中でpopしてクライアントを送信する．
     """
 
-    def initialize(self, img_list):
+    def initialize(self, image_holder):
         """コンストラクタ
 
         @param img_list 画像のリスト
@@ -50,10 +71,12 @@ class WSPopHandler(tornado.websocket.WebSocketHandler):
         異なってしまうので，入れ物を用意する必要があり，とりあえずリストにした．
         """
         self.state = True
-        self.img_list = img_list
+        self.image_holder = image_holder
+        self.index = None
 
     def open(self, index):
         print index
+        self.index = index
         # 送信スレッドの作成
         t = threading.Thread(target=self.loop)
         t.setDaemon(True)
@@ -61,10 +84,10 @@ class WSPopHandler(tornado.websocket.WebSocketHandler):
 
     def loop(self):
         """メインスレッドと非同期でクライアントに画像を送りつける"""
-        # TODO: use index
         while self.state:
-            if self.img_list:
-                self.write_message(self.img_list.pop(), binary=True)
+            buf = self.image_holder.pop(self.index)
+            if buf:
+                self.write_message(buf, binary=True)
             time.sleep(0.05)
 
     def on_close(self):
@@ -77,26 +100,29 @@ class WSPopHandler(tornado.websocket.WebSocketHandler):
 class WSPushHandler(tornado.websocket.WebSocketHandler):
     """Piからの画像を受け取るハンドラ
 
-    /recieve に対応．
+    /push に対応．
 
     画像はbase64でエンコードされて送られてくる（on_messageでバイナリで
     受け取る方法がわからなかったため）．受信したらデコードしてWSSendHandlerと
     共通のスタックへ積んであげる．
     """
-    def initialize(self, img_list):
-        self.img_list = img_list
+    def initialize(self, image_holder):
+        self.image_holder = image_holder
+        self.index = None
 
     def open(self, index):
         print("open: " + self.request.remote_ip)
+        self.index = index
 
     def on_message(self, msg):
         """base64で映像を受け取ってデコードしてスタックへ入れる"""
         # TODO: use index
-        self.img_list.append(base64.b64decode(msg))
+        buf = base64.b64decode(msg)
+        self.image_holder.push(self.index, buf)
 
     def on_close(self):
         self.close()
-        self.img_list.append(open("./static/img/default.jpg", "rb").read())
+        self.image_holder.push(self.index, None)
         print(self.request.remote_ip, ": connection closed")
 
 
@@ -104,18 +130,15 @@ def main(argv):
     argv = gflags.FLAGS(argv)
     print("start!")
 
-    # 画像の受け渡しをするスタックとして使うリスト
-    img_list = []
-
     # 初期画像
-    img_list = [open("./static/img/default.jpg", "rb").read()]
+    holder = ImageHolder()
 
     # ハンドラの登録
     # ２つのハンドラに同じimg_listを渡しているのに注目！
     handlers = [
-        (r"/([0-9a-zA-Z]+)", HttpHandler),
-        (r"/pop/([0-9a-zA-Z]+)", WSPopHandler, dict(img_list=img_list)),
-        (r"/push/([0-9a-zA-Z]+)", WSPushHandler, dict(img_list=img_list)),
+        (r"/watch/([0-9a-zA-Z]+)", HttpHandler),
+        (r"/pop/([0-9a-zA-Z]+)", WSPopHandler, dict(image_holder=holder)),
+        (r"/push/([0-9a-zA-Z]+)", WSPushHandler, dict(image_holder=holder)),
     ]
     settings = dict(
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
