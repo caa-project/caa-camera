2e7bca61c80839dc88bd3db98839d90d5966ca0b
Ryo Murakami <integral14@gmail.com>
2014-11-19 12:11:50 +0900

    camera_streaming

--- camera-client/camera.py
+++ camera-client/camera.py
@@ -6,7 +6,6 @@ import gflags
 import time
 import picamera
 import sys
-import threading
 import websocket
 
 FLAGS = gflags.FLAGS
@@ -16,71 +15,31 @@ gflags.DEFINE_integer("width", 320, "Width of movie.")
 gflags.DEFINE_integer("height", 240, "Height of movie.")
 gflags.DEFINE_integer("fps", 10, "fps")
 gflags.DEFINE_integer("quality", 20, "Quality")
+gflags.DEFINE_bool("vflip", True, "Virtical flip")
+gflags.DEFINE_bool("hflip", True, "Horizontal flip")
 
 
-def create_camera(width, height, fps):
+def create_camera(width, height, vflip, hflip):
     """カメラを適当に設定して作るファクトリ
     """
     camera = picamera.PiCamera()
     camera.resolution = (width, height)
-    camera.framerate = fps
-    camera.stream = io.BytesIO()    # ストリームIO
-    camera.vflip = True             # 垂直反転
-    camera.hflip = True             # 水平反転
+    camera.vflip = vflip            # 垂直反転
+    camera.hflip = hflip            # 水平反転
     time.sleep(2)
     return camera
 
 
-class CameraThread():
-
-    def __init__(self, width, height, fps, quality):
-        print "initializing camera..."
-        self.stop_event = threading.Event()
-        self.camera = create_camera(width, height, fps)
-        print "done"
-
-        self.quality = quality
-        self.send_thread = threading.Thread(target=self.loop)
-        self.send_thread.setDaemon(True)
-
-    def start(self, ws):
-        self.ws = ws
-        self.send_thread.start()
-
-    def stop(self):
-        self.stop_event.set()
-
-    def loop(self):
-        for foo in self.camera.capture_continuous(
-                self.camera.stream,
-                "jpeg", use_video_port=True, quality=self.quality):
-            self.camera.stream.seek(0)
-            self.ws.send(self.camera.stream.read())
-            self.camera.stream.seek(0)
-            self.camera.stream.truncate()
-            if self.stop_event.is_set():
-                break
-
-ct = CameraThread(width=FLAGS.width, height=FLAGS.height, fps=FLAGS.fps,
-                  quality=FLAGS.quality)
-
-
-def on_message(ws, msg):
-    pass
-
-
-def on_error(ws, error):
-    print error
-
-
-def on_open(ws):
-    print "### open ###"
-    ct.start(ws)
-
-
-def on_close(ws):
-    print "### close ###"
-    ct.stop()
+def camera_streaming(ws, camera):
+    stream = io.BytesIO()
+    SPF = 1.0 / FLAGS.fps   # second per frame
+    for foo in camera.capture_continuous(
+            stream, "jpeg", use_video_port=True, quality=FLAGS.quality):
+        stream.seek(0)
+        ws.send_binary(stream.read())
+        stream.seek(0)
+        stream.truncate()
+        time.sleep(SPF)
 
 
 def main(argv):
@@ -89,16 +48,17 @@ def main(argv):
     # websocket.enableTrace(True)
     while True:
         try:
-            ws = websocket.WebSocketApp(FLAGS.server,
-                                        on_message=on_message,
-                                        on_error=on_error,
-                                        on_close=on_close)
-            ws.on_open = on_open
-            ws.run_forever()
-            time.sleep(1)       # 再接続の試行までのインターバル
+            ws = websocket.create_connection(FLAGS.server)
+            camera = create_camera(FLAGS.width, FLAGS.height, FLAGS.fps,
+                                   FLAGS.vflip, FLAGS.hflip)
+            camera_streaming(ws, camera)
+
         except KeyboardInterrupt:
             print "abort"
             break
+        except Exception as e:
+            print str(e)
+            time.sleep(1)       # 再接続の試行までのインターバル
 
 
 if __name__ == '__main__':
